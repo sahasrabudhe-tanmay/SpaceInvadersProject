d223bd40c8768a5792eeb7079aeb5cb97e92d7fa
Tanmay Sahasrabudhe <tanmay.sahasrabudhe@gmail.com>
2017-07-30 10:14:31 +0530

    Initial Commit

---
+++ Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour {
+
+    public float speed = 30;
+    //public GameObject theBullet;
+
+    void FixedUpdate()
+    {
+        float horzMove = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horzMove, 0) * speed;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+
+        }
+	}
+}
